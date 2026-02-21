35a6870e8b03290ea8183b340c8ea090ff206a4b
Panom M <p.makuei@alustudent.com>
2026-02-21 11:45:35 +0300

    issues issues

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -16,10 +16,9 @@ def top_ten(subreddit):
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
-            print(None)
             return
         posts = response.json().get("data", {}).get("children", [])
         for post in posts:
             print(post.get("data", {}).get("title"))
     except Exception:
-        print(None)
+        return
