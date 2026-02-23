757f98d72812e8b1498ea3f13c65c4a7313bea56
Panom M <p.makuei@alustudent.com>
2026-02-23 12:52:27 -0800

    Changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
+1-top_ten.py
+
 Queries the Reddit API and prints the titles of the first
-10 hot posts for a given subreddit.
+10 hot posts for a given subreddit
 """
 import requests
 
